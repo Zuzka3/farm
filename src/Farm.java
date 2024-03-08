0770d830182cb456d2688e0c32da0a1ade209b74
Zuzka3 <z.pospisilova06@gmail.com>
2024-03-08 13:16:29 +0100

    write method to add flowers

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Farm {
 
+private List<Flower> flowers;
+private List<Animal> animals;
+private double land;
+private Barn barn;
+private double money;
+private double landLeft;
+
+
+    public Farm() {
+        this.flowers = new ArrayList<>();
+        this.animals = new ArrayList<>();
+        land = 100.0;
+        this.barn = new Barn();
+        this.money = 1000000.0;
+        this.landLeft = land;
+
+    }
+
+    public void plant (Flower flower){
+        if(landLeft < flower.neededArea) throw new RuntimeException("you cant do it");
+        if (flowers.size() < 5){
+            flowers.add(flower);
+            land -= flower.neededArea;
+        }
+        throw new RuntimeException("you cant do it");
+    }
 
 
 }
