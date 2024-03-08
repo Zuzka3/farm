c7d204db1ef0cc0e56e457f6ed3f1d1191b495dc
Zuzka3 <z.pospisilova06@gmail.com>
2024-03-08 13:32:57 +0100

    added another flower

--- src/Carrot.java
+++ src/Carrot.java
@@ -1,5 +1,11 @@
-public class Carrot {
+public class Carrot extends Flower {
 
+    public Carrot(String name, double price, double neededArea, double chanceOfGrowth){
+         super(name, price, neededArea, chanceOfGrowth);
+    }
 
+    public void waterPlant(){
+        chanceOfGrowth += 0.1;
+    }
 
 }
